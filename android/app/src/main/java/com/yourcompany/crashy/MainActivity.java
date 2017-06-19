7309e13fc27b6d540b90dc85a54bd91ef6238852
Yegor Jbanov <yjbanov@google.com>
2017-06-19 15:39:01 -0700

    crash in a Java call

--- android/app/src/main/java/com/yourcompany/crashy/MainActivity.java
+++ android/app/src/main/java/com/yourcompany/crashy/MainActivity.java
@@ -3,12 +3,22 @@ package com.yourcompany.crashy;
 import android.os.Bundle;
 
 import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
 
-public class MainActivity extends FlutterActivity {
+public class MainActivity extends FlutterActivity implements MethodCallHandler {
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
+    new MethodChannel(this.getFlutterView(), "crashy-custom-channel")
+        .setMethodCallHandler(this);
+  }
+
+  @Override
+  public void onMethodCall(MethodCall methodCall, Result result) {
+    throw new IllegalStateException("This is Java exception");
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,25 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:isolate';
 
-void main() {
-  runApp(new CrashyApp());
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+dynamic main() async {
+  FlutterError.onError = (FlutterErrorDetails details) {
+    print('FlutterError.onError caught: $details');
+  };
+
+  final ultimateErrorCatcher = new RawReceivePort((dynamic error) {
+    print('Isolate.current.addErrorListener caught: $error');
+  });
+  Isolate.current.addErrorListener(ultimateErrorCatcher.sendPort);
+
+  runZoned<Future<Null>>(() async {
+    runApp(new CrashyApp());
+  }, onError: (error, stackTrace) {
+    print('Zone caught: $error\n$stackTrace');
+  });
 }
 
 class CrashyApp extends StatelessWidget {
@@ -33,6 +51,14 @@ class MyHomePage extends StatelessWidget {
               elevation: 1.0,
               onPressed: () {
                 throw new StateError('This is a Dart exception.');
+              },
+            ),
+            new RaisedButton(
+              child: new Text('Java exception'),
+              elevation: 1.0,
+              onPressed: () async {
+                final channel = const MethodChannel('crashy-custom-channel');
+                await channel.invokeMethod('blah');
               },
             ),
           ],
